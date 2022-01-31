bf9b31f7ad1d5cd37bbe5c9aa72180ded4a2322b
Christian Monrad Mortensen <chri615w@edu.sde.dk>
2022-01-31 10:15:36 +0100

    Made the thing that a guy said was an changled. But I dont get how it was an changled.

---
+++ Fis_Bum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fis_Bum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fis = 3;
+            int bum = 5;
+            for (int i = 1; i < 100; i++)
+            {
+                string line = "";
+                if (i % fis == 0)
+                {
+                    line += "Fis";
+                }
+                if (i % bum == 0)
+                {
+                    line += "Bum";
+                }
+                if (line == "")
+                {
+                    Console.WriteLine(i);
+                }
+                else 
+                { 
+                    Console.WriteLine(line); 
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
